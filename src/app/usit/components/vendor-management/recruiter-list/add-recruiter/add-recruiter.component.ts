1e03de7b48fc54566ff04f58722feda3d684c88b
praneethreddy-julakanti <praneethreddyjulakanti@gmail.com>
2023-11-07 19:10:46 +0530

    recruiter list changes

--- src/app/usit/components/vendor-management/recruiter-list/add-recruiter/add-recruiter.component.ts
+++ src/app/usit/components/vendor-management/recruiter-list/add-recruiter/add-recruiter.component.ts
@@ -1,10 +1,9 @@
 import { Component, Inject, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { VendorService } from 'src/app/usit/services/vendor.service';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
-import { SnackBarService } from 'src/app/services/snack-bar.service';
+import { ISnackBarData, SnackBarService } from 'src/app/services/snack-bar.service';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatNativeDateModule } from '@angular/material/core';
@@ -17,6 +16,7 @@ import { SearchPipe } from 'src/app/pipes/search.pipe';
 import { Vms } from 'src/app/usit/models/vms';
 import { MatCardModule } from '@angular/material/card';
 import { NgxMatIntlTelInputComponent } from 'ngx-mat-intl-tel-input';
+import { RecruiterService } from 'src/app/usit/services/recruiter.service';
 
 @Component({
   selector: 'app-add-recruiter',
@@ -42,13 +42,13 @@ import { NgxMatIntlTelInputComponent } from 'ngx-mat-intl-tel-input';
 })
 export class AddRecruiterComponent implements OnInit {
   entity = new Vms();
-  vendorForm: any = FormGroup;
+  recruiterForm: any = FormGroup;
   submitted = false;
   rolearr: any = [];
   cityarr: any = [];
   pinarr: any = [];
   statearr: any = [];
-  private vendorServ = inject(VendorService);
+  private recruiterServ = inject(RecruiterService);
   private snackBarServ = inject(SnackBarService);
   private router = inject(Router);
   private formBuilder = inject(FormBuilder);
@@ -58,75 +58,128 @@ export class AddRecruiterComponent implements OnInit {
     public dialogRef: MatDialogRef<AddRecruiterComponent>
   ) {}
   ngOnInit(): void {
-    this.iniVendorForm();
+    if (this.data.actionName === 'edit-recruiter') {
+      this.iniRecruiterForm(this.data.RecruiterData);
+      console.log(this.data.RecruiterData);
+    } else {
+      this.iniRecruiterForm(null);
+    }
   }
 
   /**
-   * initializes vendor Form
+   * initializes Recruiter Form
    */
-  private iniVendorForm() {
-    this.vendorForm = this.formBuilder.group(
+  private iniRecruiterForm(recruiterData: any) {
+    this.recruiterForm = this.formBuilder.group(
       {
-        company: ['', [Validators.required]],
-        fedid: [this.vendorForm.fedid],
-        vendortype: ['', Validators.required],
-        companytype: ['', Validators.required],
-        tyretype: [''],
-        client: [this.entity.client],
-        addedby: [this.entity.addedby],
-        updatedby: [this.entity.updatedby],
-        details: [this.vendorForm.details],
-        staff: [this.entity.staff],
-        revenue: [this.entity.revenue],
-        website: [this.entity.website],
-        facebook: [this.entity.facebook],
-        industrytype: [this.entity.industrytype],
-        linkedinid: [this.entity.linkedinid],
-        twitterid: [this.entity.twitterid],
+        autoInput: [recruiterData ? recruiterData.company: '', Validators.required],
+        recruiter: [recruiterData ? recruiterData.recruiter: ''],
+        email: [recruiterData ? recruiterData.email: '', [Validators.required, Validators.email, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')]],
+        usnumber: [recruiterData ? recruiterData.usnumber: ''],
+        contactnumber: [recruiterData ? recruiterData.usnumber: ''],
+        extension: [recruiterData ? recruiterData.extension: ''],
+        recruitertype: [recruiterData ? recruiterData.recruitertype: '', Validators.required],
+        details: [recruiterData ? recruiterData.details: ''],
+        addedby: [recruiterData ? recruiterData.addedby: ''],
+        updatedby: [recruiterData ? recruiterData.updatedby: ''],
+        vendor: this.formBuilder.group({
+          vmsid: [this.recruiterForm.vmsid],
+        }),
         user: this.formBuilder.group({
           userid: localStorage.getItem('userid'),
-        }),
-        headquerter: ['', Validators.required],
-
-          // edit form
-          vmsid: [this.entity.vmsid],
-          vms_stat: [this.entity.vms_stat],
+        })
       }
     );
+    if(this.data.actionName === 'edit-recruiter') {
+      this.recruiterForm.addControl('recid', new FormControl(recruiterData ? recruiterData.recid: ''));
+      this.recruiterForm.addControl('status', new FormControl(recruiterData ? recruiterData.status: ''));
+      this.recruiterForm.addControl('remarks', new FormControl(recruiterData ? recruiterData.remarks: ''));
+      this.recruiterForm.addControl('rec_stat', new FormControl(recruiterData ? recruiterData.rec_stat: ''));
+      console.log(this.recruiterForm.value)
+    }
     //this.validateControls()
   }
 
 
   onSubmit(){
-    if (this.vendorForm.invalid) {
-      // this.blur = "enable"
-      //this.displayFormErrors();
+    const dataToBeSentToSnackBar: ISnackBarData = {
+      message: '',
+      duration: 2500,
+      verticalPosition: 'top',
+      horizontalPosition: 'center',
+      direction: 'above',
+      panelClass: ['custom-snack-success'],
+    };
+    if (this.recruiterForm.invalid) {
+      //this.blur = "enable"
+      this.displayFormErrors();
       return;
     }
     else {
       // this.blur = "Active"
     }
-    // this.ngxService.start();
-   // console.log(JSON.stringify(this.form.value, null, 2));
-    this.vendorServ.registerEntity(this.vendorForm.value)
+    console.log(this.data.actionName+" recruiterForm.value",this.recruiterForm.value);
+    this.recruiterServ.addOrUpdateRecruiter(this.recruiterForm.value, this.data.actionName)
       .subscribe(
         (data: any) => {
-          // this.ngxService.stop();
           // this.blur = "Active";
           if (data.status == 'success') {
-            // alertify.success("Vendor Added successfully");
-            // this.form.reset();
-            // this.router.navigate(['list-vendor']);
-            this.dialogRef.close();
+            dataToBeSentToSnackBar.message =
+              this.data.actionName === 'add-recruiter'
+                ? 'Recruiter added successfully'
+                : 'Recruiter updated successfully';
+            this.snackBarServ.openSnackBarFromComponent(dataToBeSentToSnackBar);
+            this.recruiterForm.reset();
           }
           else {
-            // this.ngxService.stop();
             // this.blur = "enable"
-            // this.message = data.message;
-            // alertify.error("Vendor Already Exists");
+            dataToBeSentToSnackBar.message =
+              this.data.actionName === 'add-recruiter'
+                ? 'Recruiter addition is failed'
+                : 'Recruiter updation is failed';
+            dataToBeSentToSnackBar.panelClass = ['custom-snack-failure'];
+            this.snackBarServ.openSnackBarFromComponent(dataToBeSentToSnackBar);
           }
-        }
+          this.dialogRef.close();
+        },
+        // error: (err) => {
+        //   //this.blur = 'enable';
+        //   dataToBeSentToSnackBar.message =
+        //     this.data.actionName === 'add-recruiter'
+        //       ? 'Employee addition is failed'
+        //       : 'Employee updation is failed';
+        //   dataToBeSentToSnackBar.panelClass = ['custom-snack-failure'];
+        //   this.snackBarServ.openSnackBarFromComponent(dataToBeSentToSnackBar);
+        // },
       );
+  }
+
+  displayFormErrors() {
+    Object.keys(this.recruiterForm.controls).forEach((field) => {
+      const control = this.recruiterForm.get(field);
+      if (control && control.invalid) {
+        control.markAsTouched();
+      }
+    });
+  }
+
+  emailDuplicate(event: any) {
+    const email = event.target.value;
+    this.recruiterServ.duplicatecheckEmail(email).subscribe(
+      (response: any) => {
+        if (response.status == 'success') {
+          //this.message = '';
+        }
+        else if (response.status == 'duplicate') {
+          const cn = this.recruiterForm.get('email');
+          cn.setValue('');
+          //this.message = 'Record already available with given Mail address';
+        }
+        else {
+          //alertify.error("Internal Server Error");
+        }
+      }
+    )
   }
 
 
--- src/app/usit/components/vendor-management/recruiter-list/recruiter-list.component.ts
+++ src/app/usit/components/vendor-management/recruiter-list/recruiter-list.component.ts
@@ -20,9 +20,11 @@ import {
 import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { DialogService } from 'src/app/services/dialog.service';
-import { ISnackBarData, SnackBarService } from 'src/app/services/snack-bar.service';
+import {
+  ISnackBarData,
+  SnackBarService,
+} from 'src/app/services/snack-bar.service';
 import { Recruiter } from 'src/app/usit/models/recruiter';
-import { VendorService } from 'src/app/usit/services/vendor.service';
 import { StatusComponent } from 'src/app/dialogs/status/status.component';
 import { ConfirmComponent } from 'src/app/dialogs/confirm/confirm.component';
 import { IConfirmDialogData } from 'src/app/dialogs/models/confirm-dialog-data';
@@ -32,7 +34,7 @@ import { RecruiterService } from 'src/app/usit/services/recruiter.service';
 @Component({
   selector: 'app-recruiter-list',
   standalone: true,
-  imports: [ 
+  imports: [
     MatTableModule,
     MatIconModule,
     MatButtonModule,
@@ -44,13 +46,21 @@ import { RecruiterService } from 'src/app/usit/services/recruiter.service';
     CommonModule,
   ],
   templateUrl: './recruiter-list.component.html',
-  styleUrls: ['./recruiter-list.component.scss']
+  styleUrls: ['./recruiter-list.component.scss'],
 })
-export class RecruiterListComponent implements OnInit{
+export class RecruiterListComponent implements OnInit {
   dataTableColumns: string[] = [
-    'SerialNum', 'Company', 'RecruiterName',
-    'PhoneNumber','Email','AddedBy', 'AddedOn',
-    'LastUpdated','Status', 'Action','Approve/Reject'
+    'SerialNum',
+    'Company',
+    'RecruiterName',
+    'PhoneNumber',
+    'Email',
+    'AddedBy',
+    'AddedOn',
+    'LastUpdated',
+    'Status',
+    'Action',
+    'Approve/Reject',
   ];
 
   dataSource = new MatTableDataSource([]);
@@ -76,28 +86,27 @@ export class RecruiterListComponent implements OnInit{
   recrData: Recruiter[] = [];
   entity: any[] = [];
   totalItems: any;
+
   // pagination code
   page: number = 1;
   itemsPerPage = 50;
-  AssignedPageNum !: any;
-  field = "empty";
+  AssignedPageNum!: any;
+  field = 'empty';
 
   ngOnInit(): void {
     this.hasAcces = localStorage.getItem('role');
     this.loginId = localStorage.getItem('userid');
     this.department = localStorage.getItem('department');
     this.AssignedPageNum = localStorage.getItem('rnum');
-    console.log(this.hasAcces, this.loginId, this.department, this.AssignedPageNum);
-    // //this.getall();
-    // if (this.AssignedPageNum == null) {
-    //   this.getAllData();
-    // }
-    // else {
-    //   this.gty(this.AssignedPageNum);
-    //   this.page = this.AssignedPageNum;
-    // }
-
-   this.getAllData();
+    //this.getall();
+    if (this.AssignedPageNum == null) {
+      this.getAllData();
+    }
+    else {
+      this.gty(this.AssignedPageNum);
+      this.page = this.AssignedPageNum;
+    }
+    this.getAllData();
   }
 
   ngAfterViewInit() {
@@ -105,28 +114,58 @@ export class RecruiterListComponent implements OnInit{
     this.dataSource.paginator = this.paginator;
   }
 
+  /**
+   * get all recruiter data
+   * @returns recruiter data
+   */
+  getAllRecruiters() {
+    return this.recruiterServ
+      .getAllRecruiters(this.hasAcces, this.loginId)
+      .subscribe((response: any) => {
+        console.log('recruiter.data', response.data);
+        if (response.data) {
+          this.dataSource.data = response.data;
+        }
+      });
+  }
+
   getAllData() {
-    return this.recruiterServ. getAllRecruitersPagination(this.hasAcces, this.loginId, 1, this.itemsPerPage, this.field).subscribe(
-      ((response: any) => {
+    return this.recruiterServ
+      .getAllRecruitersPagination(
+        this.hasAcces,
+        this.loginId,
+        1,
+        this.itemsPerPage,
+        this.field
+      )
+      .subscribe((response: any) => {
         this.datarr = response.data.content;
         this.dataSource.data = response.data.content;
         console.log(this.dataSource.data);
         // for serial-num {}
-        this.dataSource.data.map( (x:any, i)=> {x.serialNum = i+1});
+        this.dataSource.data.map((x: any, i) => {
+          x.serialNum = i + 1;
+        });
         this.totalItems = response.data.totalElements;
-
-      })
-    );
+      });
   }
   gty(page: any) {
     this.assignToPage = page;
-    return this.recruiterServ. getAllRecruitersPagination(this.hasAcces, this.loginId, page, this.itemsPerPage, this.field).subscribe(
-      ((response: any) => {
+    return this.recruiterServ
+      .getAllRecruitersPagination(
+        this.hasAcces,
+        this.loginId,
+        page,
+        this.itemsPerPage,
+        this.field
+      )
+      .subscribe((response: any) => {
         this.datarr = response.data.content;
-        this.dataSource.data.map( (x:any, i)=> {x.serialNum = i+1});
+        this.dataSource.data.map((x: any, i) => {
+          x.serialNum = i + 1;
+        });
         this.totalItems = response.data.totalElements;
-      })
-    );
+      });
   }
 
   /**
@@ -151,34 +190,28 @@ export class RecruiterListComponent implements OnInit{
 
     const isAsc = sortDirection === 'asc';
     this.dataSource.data = this.dataSource.data.sort((a: any, b: any) => {
+      const serialNumA = (a.serialNum || '').toString();
+      const serialNumB = (b.serialNum || '').toString();
       switch (activeSortHeader) {
         case 'SerialNum':
-          return (
-            (isAsc ? 1 : -1) *
-            (a.serialNum || '').localeCompare(b.serialNum || '')
-          );
+          return (isAsc ? 1 : -1) * serialNumA.localeCompare(serialNumB);
         case 'Company':
           return (
             (isAsc ? 1 : -1) * (a.company || '').localeCompare(b.company || '')
           );
-        case 'HeadQuarter':
-          return (
-            (isAsc ? 1 : -1) * (a.headquerter || '').localeCompare(b.headquerter || '')
-          );
-        case 'Fed-Id':
+        case 'RecruiterName':
           return (
             (isAsc ? 1 : -1) *
-            (a.fedid || '').localeCompare(b.fedid || '')
+            (a.recruiter || '').localeCompare(b.recruiter || '')
           );
-        case 'VendorType':
+        case 'PhoneNumber':
           return (
             (isAsc ? 1 : -1) *
-            (a.vendortype || '').localeCompare(b.vendortype || '')
+            (a.usnumber || '').localeCompare(b.usnumber || '')
           );
-        case 'TierType':
+        case 'Email':
           return (
-            (isAsc ? 1 : -1) *
-            (a.tyretype || '').localeCompare(b.tyretype || '')
+            (isAsc ? 1 : -1) * (a.email || '').localeCompare(b.email || '')
           );
         case 'AddedBy':
           return (
@@ -186,22 +219,23 @@ export class RecruiterListComponent implements OnInit{
             (a.pseudoname || '').localeCompare(b.pseudoname || '')
           );
         case 'AddedOn':
-            return (
-              (isAsc ? 1 : -1) *
-              (a.createddate || '').localeCompare(b.createddate || '')
-            );
+          return (
+            (isAsc ? 1 : -1) *
+            (a.createddate || '').localeCompare(b.createddate || '')
+          );
         case 'LastUpdated':
-            return (
-              (isAsc ? 1 : -1) *
-              (a.updateddate || '').localeCompare(b.updateddate || '')
-            );
+          return (
+            (isAsc ? 1 : -1) *
+            (a.updateddate || '').localeCompare(b.updateddate || '')
+          );
         case 'Status':
           return (
             (isAsc ? 1 : -1) * (a.status || '').localeCompare(b.status || '')
           );
         case 'Approve/Reject':
           return (
-            (isAsc ? 1 : -1) * (a.vms_stat || '').localeCompare(b.vms_stat || '')
+            (isAsc ? 1 : -1) *
+            (a.rec_stat || '').localeCompare(b.rec_stat || '')
           );
         default:
           return 0;
@@ -211,12 +245,13 @@ export class RecruiterListComponent implements OnInit{
 
   /**
    * add
+   * @param recruiter
    */
   addRecruiter() {
     const actionData = {
-      title: 'Add Vendor',
-      vendorData: null,
-      actionName: 'add',
+      title: 'Add Recruiter',
+      recruiterData: null,
+      actionName: 'add-recruiter',
     };
     const dialogConfig = new MatDialogConfig();
     dialogConfig.width = '65vw';
@@ -225,12 +260,224 @@ export class RecruiterListComponent implements OnInit{
     dialogConfig.panelClass = 'add-recruiter';
     dialogConfig.data = actionData;
 
-    this.dialogServ.openDialogWithComponent(AddRecruiterComponent, dialogConfig);
+    this.dialogServ.openDialogWithComponent(
+      AddRecruiterComponent,
+      dialogConfig
+    );
+  }
+  /**
+   * edit
+   * @param recruiter
+   */
+  editRecruiter(recruiter: any) {
+    const actionData = {
+      title: 'Update Recruiter',
+      RecruiterData: recruiter,
+      actionName: 'edit-recruiter',
+    };
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '65vw';
+    //dialogConfig.height = '100vh';
+    dialogConfig.panelClass = 'edit-recruiter';
+    dialogConfig.data = actionData;
+    this.dialogServ.openDialogWithComponent(
+      AddRecruiterComponent,
+      dialogConfig
+    );
+  }
+
+  /**
+   * delete
+   * @param recruiter
+   */
+  deleteRecruiter(recruiter: any) {
+    const dataToBeSentToDailog: Partial<IConfirmDialogData> = {
+      title: 'Confirmation',
+      message: 'Are you sure you want to delete?',
+      confirmText: 'Yes',
+      cancelText: 'No',
+      actionData: recruiter,
+    };
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '400px';
+    dialogConfig.height = 'auto';
+    dialogConfig.disableClose = false;
+    dialogConfig.panelClass = 'delete-recruiter';
+    dialogConfig.data = dataToBeSentToDailog;
+    const dialogRef = this.dialogServ.openDialogWithComponent(
+      ConfirmComponent,
+      dialogConfig
+    );
+
+    // call delete api after  clicked 'Yes' on dialog click
+    dialogRef.afterClosed().subscribe({
+      next: (resp) => {
+        if (dialogRef.componentInstance.allowAction) {
+          const dataToBeSentToSnackBar: ISnackBarData = {
+            message: 'Status updated successfully!',
+            duration: 1500,
+            verticalPosition: 'top',
+            horizontalPosition: 'center',
+            direction: 'above',
+            panelClass: ['custom-snack-success'],
+          };
+
+          // this.recruiterServ
+          //   .deleteEntity(recruiter.userid)
+          //   .subscribe((response: any) => {
+          //     if (response.status == 'Success') {
+          //       this.getAllRecruiters();
+          //       dataToBeSentToSnackBar.message =
+          //         'Recruiter Deleted successfully';
+          //       this.snackBarServ.openSnackBarFromComponent(
+          //         dataToBeSentToSnackBar
+          //       );
+          //     } else {
+          //       dataToBeSentToSnackBar.panelClass = ['custom-snack-failure'];
+          //       dataToBeSentToSnackBar.message = 'Record Deletion failed';
+          //     }
+          //   });
+        }
+      },
+    });
+  }
+
+  /**
+   * on status update
+   * @param recruiter
+   */
+  onStatusUpdate(recruiter: any) {
+    const dataToBeSentToDailog = {
+      title: 'Status Update',
+      updateText: recruiter.status !== 'Active' ? 'activating' : 'in-activating',
+      type: 'Recruiter',
+      buttonText: 'Update',
+      actionData: recruiter,
+    };
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '400px';
+    dialogConfig.height = 'auto';
+    dialogConfig.disableClose = false;
+    dialogConfig.panelClass = 'update-recruiter-status';
+    dialogConfig.data = dataToBeSentToDailog;
+
+    const dialogRef = this.dialogServ.openDialogWithComponent(
+      StatusComponent,
+      dialogConfig
+    );
+
+    dialogRef.afterClosed().subscribe({
+      next: (resp) => {
+        if (dialogRef.componentInstance.submitted) {
+          const dataToBeSentToSnackBar: ISnackBarData = {
+            message: 'Status updated successfully!',
+            duration: 1500,
+            verticalPosition: 'top',
+            horizontalPosition: 'center',
+            direction: 'above',
+            panelClass: ['custom-snack-success'],
+          };
+          recruiter.remarks = dialogRef.componentInstance.remarks;
+          this.recruiterServ
+            .changeStatus2(recruiter.id, recruiter.status, recruiter.remarks)
+            .subscribe((response: any) => {
+              if (response.status == 'Success') {
+                this.gty(this.page);
+                dataToBeSentToSnackBar.message = 'Status updated successfully';
+              } else {
+                dataToBeSentToSnackBar.panelClass = ['custom-snack-failure'];
+                dataToBeSentToSnackBar.message = 'Status update failed';
+              }
+              this.snackBarServ.openSnackBarFromComponent(
+                dataToBeSentToSnackBar
+              );
+            });
+        }
+      },
+    });
+  }
+
+  // approve initiate reject
+  onApproveOrRejectRecruiter(recruiter: any, rejectRecruiter = false) {
+    if (recruiter.rec_stat !== 'Approved') {
+      const dataToBeSentToSnackBar: ISnackBarData = {
+        message: 'Status updated successfully!',
+        duration: 1500,
+        verticalPosition: 'top',
+        horizontalPosition: 'center',
+        direction: 'above',
+        panelClass: ['custom-snack-success'],
+      };
+      if (this.department == recruiter.ctype) {
+        // alertify.error("Your not Authorized to approve the recruiter");
+        dataToBeSentToSnackBar.message =
+          'You are not Authorized to approve the Recruiter';
+        dataToBeSentToSnackBar.panelClass = ['custom-snack-failure'];
+        this.snackBarServ.openSnackBarFromComponent(dataToBeSentToSnackBar);
+        return;
+      }
+
+      const dataToBeSentToDailog: Partial<IConfirmDialogData> = {
+        title:
+          recruiter.rec_stat == 'Initiated' && !rejectRecruiter
+            ? 'Approve Recruiter'
+            : 'Reject Recruiter',
+        message:
+          recruiter.rec_stat == 'Initiated' && !rejectRecruiter
+            ? 'Are you sure you want to Approve the Recruiter ?'
+            : 'Are you sure you want to Reject the Recruiter ?',
+        confirmText: 'Yes',
+        cancelText: 'No',
+        actionData: recruiter,
+      };
+      const dialogConfig = new MatDialogConfig();
+      dialogConfig.width = 'fit-content';
+      dialogConfig.height = 'auto';
+      dialogConfig.disableClose = false;
+      dialogConfig.panelClass = `${recruiter.rec_stat == 'Initiated' && !rejectRecruiter ? 'approve' : 'reject'
+        }-recruiter`;
+      dialogConfig.data = dataToBeSentToDailog;
+      const dialogRef = this.dialogServ.openDialogWithComponent(
+        ConfirmComponent,
+        dialogConfig
+      );
+
+      const statReqObj = {
+        action: recruiter.rec_stat === 'Initiated' ? 'Approved' : 'Reject',
+        id: recruiter.id,
+        loginId: this.loginId,
+      };
+      console.log(statReqObj);
+      dialogRef.afterClosed().subscribe(() => {
+        this.recruiterServ
+          .approve(statReqObj.action, statReqObj.id, statReqObj.loginId)
+          .subscribe((response: any) => {
+            console.log(JSON.stringify(response));
+            if (response.status == 'Approved') {
+              dataToBeSentToSnackBar.message = `Recruiter ${response.data} successfully`;
+              dataToBeSentToSnackBar.panelClass = ['custom-snack-success'];
+              this.snackBarServ.openSnackBarFromComponent(
+                dataToBeSentToSnackBar
+              );
+            } else {
+              //  alertify.success("recruiter " + response.data + " successfully");
+              dataToBeSentToSnackBar.message = `Recruiter ${response.data} successfully`;
+              dataToBeSentToSnackBar.panelClass = ['custom-snack-success'];
+              this.snackBarServ.openSnackBarFromComponent(
+                dataToBeSentToSnackBar
+              );
+            }
+            this.gty(this.page);
+          });
+      });
+      // after closing popup
+    }
+    return;
   }
 
   /**
    * handle page event - pagination
-   * @param endor
+   * @param recruiter
    */
   handlePageEvent(e: PageEvent) {
     this.pageEvent = e;
@@ -238,5 +485,4 @@ export class RecruiterListComponent implements OnInit{
     this.pageSize = e.pageSize;
     this.pageIndex = e.pageIndex;
   }
-  
 }
