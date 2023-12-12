7b92d6f461d39594ede2b222f1f338fd6ec65b66
Kiran-Idsl <kiranjava0101@gmail.com>
2023-12-12 20:05:58 +0530

    consultant list and file management service changes

--- src/app/usit/services/file-management.service.ts
+++ src/app/usit/services/file-management.service.ts
@@ -16,6 +16,10 @@ export class FileManagementService {
     return this.http.post(this.apiServ.apiUrl + `auth/users/uploadMultiple/${id}`, formData, {observe: "response"});
   }
 
+  ConUploadFile(formData:any, id: number) {
+    return this.http.post(this.apiServ.apiUrl + `consultant/uploadMultiple/${id}`, formData, {observe: "response"});
+  }
+
   removefile(id: number, flg: string) {
     return this.apiServ.get(`auth/users/removefile/${id}/${flg}`);
   }
